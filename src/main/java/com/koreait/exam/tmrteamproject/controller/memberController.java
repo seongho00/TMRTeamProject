23c58b3eabe599c9e8ee83f8e465153b1b5157f8
seongho00 <tjdwns000927@gmail.com>
2025-07-09 16:59:09 +0900

    네이버 로그인 구현

--- src/main/java/com/koreait/exam/tmrteamproject/controller/memberController.java
+++ src/main/java/com/koreait/exam/tmrteamproject/controller/memberController.java
@@ -1,14 +1,27 @@
 package com.koreait.exam.tmrteamproject.controller;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.koreait.exam.tmrteamproject.service.KakaoOAuthService;
+import com.koreait.exam.tmrteamproject.service.NaverOAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
 
 @Controller
 @RequestMapping("usr/member")
@@ -19,8 +32,15 @@ public class memberController {
     @Value("${kakao.api.clientId}")
     private String kakaoClientId;
 
+    @Value("${naver.api.clientId}")
+    private String naverClientId;
+
     @Autowired
     private KakaoOAuthService kakaoOAuthService;
+    @Autowired
+    private NaverOAuthService naverOAuthService;
+
+    
 
     @GetMapping("/login")
     public String login() {
@@ -50,8 +70,27 @@ public class memberController {
     @GetMapping("/loginNaver")
     public String loginNaver() {
 
-        return "member/login";
+
+        String redirectURI = "http://localhost:8080/usr/member/naverCallback";
+        SecureRandom random = new SecureRandom();
+        String state = new BigInteger(130, random).toString();
+        String apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code" + "&client_id=" + naverClientId
+                + "&redirect_uri=" + redirectURI + "&state=" + state;
+
+        return "redirect:" + apiURL;
     }
+
+    @GetMapping("/naverCallback")
+    public String naverCallback(String code, String state) throws UnsupportedEncodingException {
+
+        String accessToken = naverOAuthService.requestAccessToken(code, state);
+
+        naverOAuthService.getUserInfo(accessToken);
+
+
+        return "redirect:../home/main";
+    }
+
 
 
 }
