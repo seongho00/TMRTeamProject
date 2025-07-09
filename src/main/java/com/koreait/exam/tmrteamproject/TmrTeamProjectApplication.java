20bd36ec6d7fad3c47c1f315f22de41ab1244e96
seongho00 <tjdwns000927@gmail.com>
2025-07-09 12:02:22 +0900

    first commit

---
+++ src/main/java/com/koreait/exam/tmrteamproject/TmrTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.tmrteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmrTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmrTeamProjectApplication.class, args);
+    }
+
+}
