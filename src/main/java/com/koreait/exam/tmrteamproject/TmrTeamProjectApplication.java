bba46217a928f3509d44584a976b5e1360d1ff26
seongho00 <tjdwns000927@gmail.com>
2025-07-11 09:23:55 +0900

    스케쥴에 따라 자동으로 문자 발송

--- src/main/java/com/koreait/exam/tmrteamproject/TmrTeamProjectApplication.java
+++ src/main/java/com/koreait/exam/tmrteamproject/TmrTeamProjectApplication.java
@@ -3,7 +3,9 @@ package com.koreait.exam.tmrteamproject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TmrTeamProjectApplication {
 
