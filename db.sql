aeba3141575a5ddd366d8aa253d8a9301d2fb8f3
seongho00 <tjdwns000927@gmail.com>
2025-07-12 18:43:28 +0900

    DB에 대전광역시 행정동 자동 insert

--- db.sql
+++ db.sql
@@ -11,3 +11,23 @@ CREATE TABLE MEMBER (
                         email VARCHAR(100) NOT NULL,
                         phoneNum VARCHAR(20)
 );
+
+
+
+CREATE TABLE areaCode (
+                          id INT(10) AUTO_INCREMENT PRIMARY KEY,
+                          regDate DATETIME NOT NULL,
+                          updateDate DATETIME NOT NULL,
+                          sidoCode CHAR(10) NOT NULL,
+                          sidoName CHAR(20) NOT NULL,
+                          sigunguCode CHAR(10) NOT NULL,
+                          sigunguName CHAR(20) NOT NULL,
+                          admDongCode CHAR(10) NOT NULL,
+                          admDongName CHAR(20) NOT NULL,
+                          fullCode CHAR(100) NOT NULL,
+                          fullName CHAR(100) NOT NULL
+);
+
+
+SELECT * FROM `member`
+
