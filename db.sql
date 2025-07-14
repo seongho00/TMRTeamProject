0a6affcf202cfa0ccc34f389dc1a4859703529d2
seongho00 <tjdwns000927@gmail.com>
2025-07-14 10:13:48 +0900

    DB 업데이트, 챗봇기능 파일생성

--- db.sql
+++ db.sql
@@ -2,25 +2,14 @@ DROP DATABASE IF EXISTS `TMRTeamProject`;
 CREATE DATABASE `TMRTeamProject`;
 USE `TMRTeamProject`;
 
-CREATE TABLE MEMBER (
-                        id INT(10) AUTO_INCREMENT PRIMARY KEY,
-                        regDate DATETIME NOT NULL,
-                        updateDate DATETIME NOT NULL,
-                        `name` VARCHAR(50) NOT NULL,
-                        loginPw VARCHAR(100) NOT NULL,
-                        email VARCHAR(100) NOT NULL,
-                        phoneNum VARCHAR(20)
-);
-
-
-CREATE TABLE floating_population (
-                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                     region_name VARCHAR(100),             -- 예: 강남구 역삼동
-                                     gender ENUM('M', 'F') NOT NULL,       -- 남(M), 여(F)
-                                     age_group VARCHAR(10) NOT NULL,       -- 예: '10대', '20대'
-                                     population_count INT NOT NULL,        -- 유동인구 수
-                                     collected_date DATE NOT NULL,         -- 데이터 수집 기준일
-                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE `member` (
+                          id INT(10) AUTO_INCREMENT PRIMARY KEY,
+                          reg_date DATETIME NOT NULL,
+                          update_date DATETIME NOT NULL,
+                          `name` VARCHAR(50) NOT NULL,
+                          login_pw VARCHAR(100) NOT NULL,
+                          email VARCHAR(100) NOT NULL,
+                          phone_num VARCHAR(20)
 );
 
 
@@ -33,3 +22,24 @@ CREATE TABLE admin_dong (
                             emd_cd CHAR(10) PRIMARY KEY,         -- 읍면동 코드 (PK)
                             emd_nm VARCHAR(20)
 );
+
+CREATE TABLE population_stat (
+                                 id INT AUTO_INCREMENT PRIMARY KEY,
+                                 emd_cd CHAR(10),             -- 읍면동 코드
+                                 total INT,
+                                 male INT,
+                                 female INT,
+                                 age_10 INT,
+                                 age_20 INT,
+                                 age_30 INT,
+                                 age_40 INT,
+                                 age_50 INT,
+                                 age_60 INT
+
+);
+
+
+SELECT * FROM admin_dong;
+SELECT * FROM population_stat;
+
+
