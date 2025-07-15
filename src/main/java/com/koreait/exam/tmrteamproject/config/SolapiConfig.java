0aa68d44bfc04f97346dbc715a30b649a7051773
hwshin1 <hwshin5044@gmail.com>
2025-07-15 17:54:35 +0900

    footer.html 추가

--- src/main/java/com/koreait/exam/tmrteamproject/config/SolapiConfig.java
+++ src/main/java/com/koreait/exam/tmrteamproject/config/SolapiConfig.java
@@ -2,11 +2,9 @@ package com.koreait.exam.tmrteamproject.config;
 
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.service.DefaultMessageService;
-import net.nurigo.sdk.message.service.MessageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class SolapiConfig {
