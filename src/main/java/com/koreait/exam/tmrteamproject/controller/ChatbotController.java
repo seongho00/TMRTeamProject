89825cdd8f1d257e97ec8e29bbebfb54a357a9da
seongho00 <tjdwns000927@gmail.com>
2025-07-15 09:52:36 +0900

    파이썬 api와 html 연결

--- src/main/java/com/koreait/exam/tmrteamproject/controller/ChatbotController.java
+++ src/main/java/com/koreait/exam/tmrteamproject/controller/ChatbotController.java
@@ -2,11 +2,14 @@ package com.koreait.exam.tmrteamproject.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+import org.json.JSONObject;
 
 @Controller
 @RequestMapping("usr/chatbot")
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class ChatbotController {
 
+    private final RestTemplate restTemplate = new RestTemplate();
 
     @GetMapping("/chat")
     public String chat() {
@@ -22,7 +26,18 @@ public class ChatbotController {
 
     @PostMapping("/sendMessage")
     @ResponseBody
-    public String sendMessage() {
-        return "sendMessage 실행됨";
+    public String sendMessage(String message) {
+        System.out.println(message);
+        String flaskUrl = "http://localhost:5000/predict?text=" + message;
+
+        try {
+            String response = restTemplate.getForObject(flaskUrl, String.class);
+            JSONObject json = new JSONObject(response);
+            return json.getString("message"); // ✅ 응답 메시지만 꺼내서 리턴
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "❌ Flask 서버 연결 실패";
+        }
+
     }
 }
