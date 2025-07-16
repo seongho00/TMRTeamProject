7867a9fa50328bdbbf696fc675a34cf765948f98
seongho00 <tjdwns000927@gmail.com>
2025-07-16 12:36:29 +0900

    지역 관련 여러 상황에서 검색 구현

--- src/main/java/com/koreait/exam/tmrteamproject/repository/PopulationStatRepository.java
+++ src/main/java/com/koreait/exam/tmrteamproject/repository/PopulationStatRepository.java
@@ -38,4 +38,92 @@ public interface PopulationStatRepository extends JpaRepository<PopulationStat, 
             """, nativeQuery = true)
     PopulationSummary findBySidoAndSigungu(@Param("sido") String sido, @Param("sigungu") String sigungu);
 
+    @Query(value = """
+            SELECT 
+                SUM(p.total) AS total,
+                SUM(p.male) AS male,
+                SUM(p.female) AS female,
+                SUM(p.age_10) AS age10,
+                SUM(p.age_20) AS age20,
+                SUM(p.age_30) AS age30,
+                SUM(p.age_40) AS age40,
+                SUM(p.age_50) AS age50,
+                SUM(p.age_60) AS age60
+            FROM population_stat p
+            JOIN admin_dong a ON p.emd_cd = a.emd_cd
+            WHERE a.sido_nm = :sido AND a.emd_nm = :emd
+            GROUP BY a.sgg_nm
+            """, nativeQuery = true)
+    List<PopulationSummary> findBySidoAndEmd(@Param("sido") String sido, @Param("emd") String emd);
+
+
+    @Query(value = """
+            SELECT 
+                SUM(p.total) AS total,
+                SUM(p.male) AS male,
+                SUM(p.female) AS female,
+                SUM(p.age_10) AS age10,
+                SUM(p.age_20) AS age20,
+                SUM(p.age_30) AS age30,
+                SUM(p.age_40) AS age40,
+                SUM(p.age_50) AS age50,
+                SUM(p.age_60) AS age60
+            FROM population_stat p
+            JOIN admin_dong a ON p.emd_cd = a.emd_cd
+            WHERE a.sgg_nm = :sigungu AND a.emd_nm = :emd
+            """, nativeQuery = true)
+    PopulationSummary findBySigunguAndEmd(@Param("sigungu") String sigungu, @Param("emd") String emd);
+
+    @Query(value = """
+            SELECT 
+                SUM(p.total) AS total,
+                SUM(p.male) AS male,
+                SUM(p.female) AS female,
+                SUM(p.age_10) AS age10,
+                SUM(p.age_20) AS age20,
+                SUM(p.age_30) AS age30,
+                SUM(p.age_40) AS age40,
+                SUM(p.age_50) AS age50,
+                SUM(p.age_60) AS age60
+            FROM population_stat p
+            JOIN admin_dong a ON p.emd_cd = a.emd_cd
+            WHERE a.sido_nm = :sido
+            """, nativeQuery = true)
+    PopulationSummary findBySido(@Param("sido") String sido);
+
+    @Query(value = """
+            SELECT 
+                SUM(p.total) AS total,
+                SUM(p.male) AS male,
+                SUM(p.female) AS female,
+                SUM(p.age_10) AS age10,
+                SUM(p.age_20) AS age20,
+                SUM(p.age_30) AS age30,
+                SUM(p.age_40) AS age40,
+                SUM(p.age_50) AS age50,
+                SUM(p.age_60) AS age60
+            FROM population_stat p
+            JOIN admin_dong a ON p.emd_cd = a.emd_cd
+            WHERE a.sgg_nm = :sigungu
+            """, nativeQuery = true)
+    PopulationSummary findBySigungu(@Param("sigungu") String sigungu);
+
+    @Query(value = """
+            SELECT 
+                SUM(p.total) AS total,
+                SUM(p.male) AS male,
+                SUM(p.female) AS female,
+                SUM(p.age_10) AS age10,
+                SUM(p.age_20) AS age20,
+                SUM(p.age_30) AS age30,
+                SUM(p.age_40) AS age40,
+                SUM(p.age_50) AS age50,
+                SUM(p.age_60) AS age60
+            FROM population_stat p
+            JOIN admin_dong a ON p.emd_cd = a.emd_cd
+            WHERE a.emd_nm = :emd
+            GROUP BY a.sgg_nm
+            """, nativeQuery = true)
+    List<PopulationSummary> findByEmd(@Param("emd") String emd);
+
 }
