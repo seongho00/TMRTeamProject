484baae5a3287c214ab35e8c7210c2edcd895c1a
seongho00 <tjdwns000927@gmail.com>
2025-07-17 11:01:28 +0900

    구글 로그인 구현

---
+++ src/main/java/com/koreait/exam/tmrteamproject/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.koreait.exam.tmrteamproject.config;
+
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // ✅ 모든 요청 허용
+                )
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/usr/home/main", true)
+                );
+
+        return http.build();
+    }
+}
