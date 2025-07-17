07074116a134667b448478f36bc9f614d33cc98d
hwshin1 <hwshin5044@gmail.com>
2025-07-17 11:08:04 +0900

    pdf 파일 읽기 및 이미지 불러오기

--- src/main/java/com/koreait/exam/tmrteamproject/controller/MarketAreaController.java
+++ src/main/java/com/koreait/exam/tmrteamproject/controller/MarketAreaController.java
@@ -1,10 +1,15 @@
 package com.koreait.exam.tmrteamproject.controller;
 
+import com.koreait.exam.tmrteamproject.service.MarketAreaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -12,8 +17,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("usr/area")
 public class MarketAreaController {
 
+    @Autowired
+    private MarketAreaService marketAreaService;
+
     @GetMapping("/page")
     public String binPage() {
         return "trade/binPage";
+    }
+
+    @GetMapping("/pdfread")
+    public String pdfread(Model model) {
+        List<String> images = marketAreaService.extractImagesFromPdf();
+        model.addAttribute("images", images);
+        String pdfText = marketAreaService.getPdfRead();
+        model.addAttribute("pdfText", pdfText);
+        return "trade/pdfread";
     }
 }
