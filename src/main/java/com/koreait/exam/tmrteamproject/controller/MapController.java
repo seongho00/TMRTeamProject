2101101decde1908fb00e4dbfbbc6b1f1f5d2799
seongho00 <tjdwns000927@gmail.com>
2025-07-21 13:21:57 +0900

    지역선택 페이지 기초 작업 중

---
+++ src/main/java/com/koreait/exam/tmrteamproject/controller/MapController.java
@@ -0,0 +1,21 @@
+package com.koreait.exam.tmrteamproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("usr/map")
+@Slf4j
+@RequiredArgsConstructor
+public class MapController {
+
+    @GetMapping("/commercialZoneMap")
+    public String commercialZoneMap() {
+
+        return "map/commercialZoneMap";
+    }
+}
