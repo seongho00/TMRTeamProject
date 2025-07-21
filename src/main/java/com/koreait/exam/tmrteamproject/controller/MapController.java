254bd3b795aebdaa08ea1d5ad1a52779608c7880
seongho00 <tjdwns000927@gmail.com>
2025-07-21 17:39:11 +0900

    시군구 선택시 해당 행정동만 선택할 수 있도록

--- src/main/java/com/koreait/exam/tmrteamproject/controller/MapController.java
+++ src/main/java/com/koreait/exam/tmrteamproject/controller/MapController.java
@@ -1,11 +1,18 @@
 package com.koreait.exam.tmrteamproject.controller;
 
+import com.koreait.exam.tmrteamproject.service.MapService;
+import com.koreait.exam.tmrteamproject.service.MemberService;
+import com.koreait.exam.tmrteamproject.vo.AdminDong;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("usr/map")
@@ -13,9 +20,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MapController {
 
+    @Autowired
+    private MapService mapService;
+
     @GetMapping("/commercialZoneMap")
     public String commercialZoneMap() {
 
         return "map/commercialZoneMap";
+    }
+
+    @GetMapping("/getEmds")
+    @ResponseBody
+    public List<AdminDong> getEmds(String sgg) {
+
+        List<AdminDong> adminDong = mapService.getEmdsBySgg(sgg);
+
+        return adminDong;
     }
 }
