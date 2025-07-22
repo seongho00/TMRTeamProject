79c4f043ff1d48dbb59af5d0e8fcd9725a9307a4
seongho00 <tjdwns000927@gmail.com>
2025-07-22 10:49:18 +0900

    대분류 클릭 시 중분류 출력

---
+++ src/main/java/com/koreait/exam/tmrteamproject/service/UpjongCodeService.java
@@ -0,0 +1,26 @@
+package com.koreait.exam.tmrteamproject.service;
+
+import com.koreait.exam.tmrteamproject.repository.AdminDongRepository;
+import com.koreait.exam.tmrteamproject.repository.UpjongCodeRepository;
+import com.koreait.exam.tmrteamproject.vo.UpjongCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UpjongCodeService {
+
+    private final UpjongCodeRepository upjongCodeRepository;
+
+    public List<UpjongCode> getUpjongCodesByMajorNm(String majorNm) {
+        return upjongCodeRepository.getUpjongCodesByMajorNm(majorNm);
+    }
+
+    public List<UpjongCode> getGroupedUpjongCodesByMajorCd(String majorCd) {
+        return upjongCodeRepository.getGroupedUpjongCodesByMajorCd(majorCd);
+    }
+}
