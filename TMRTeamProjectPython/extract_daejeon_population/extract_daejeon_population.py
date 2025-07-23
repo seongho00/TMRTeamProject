5d1e0c269eb95aca4b7859de8d7d743bb94845e4
seongho00 <tjdwns000927@gmail.com>
2025-07-23 16:57:06 +0900

    2024년도 주거인구 수집 완료

--- TMRTeamProjectPython/extract_daejeon_population/extract_daejeon_population.py
+++ TMRTeamProjectPython/extract_daejeon_population/extract_daejeon_population.py
@@ -53,6 +53,14 @@ for filename in os.listdir(input_folder):
             male_cols = [f"{age}세남자" for age in range(start, end + 1) if f"{age}세남자" in daejeon_df.columns]
             female_cols = [f"{age}세여자" for age in range(start, end + 1) if f"{age}세여자" in daejeon_df.columns]
 
+            # ✅ 60~200세 범위에는 110세이상 컬럼 수동 추가
+            if start == 60 and end == 200:
+                if "110세이상 남자" in daejeon_df.columns:
+                    male_cols.append("110세이상 남자")
+                if "110세이상 여자" in daejeon_df.columns:
+                    female_cols.append("110세이상 여자")
+
+
             # 새 컬럼 이름 (예: age_10_19_male)
             male_col = f"age_{start}_{end}_male"
             female_col = f"age_{start}_{end}_female"
@@ -63,7 +71,7 @@ for filename in os.listdir(input_folder):
             daejeon_df.loc[:, total_col] = daejeon_df[male_col] + daejeon_df[female_col]
 
         # ✅ 나이별 개별 컬럼 삭제 (이 코드 추가!)
-        cols_to_drop = [col for col in daejeon_df.columns if '세남자' in col or '세여자' in col or '110세이상 남자' in col or '110세이상 여자' in col]
+        cols_to_drop = [col for col in daejeon_df.columns if '세남자' in col or '세여자' in col or '110세이상 남자' in col or '110세이상 여자' in col or '기준연월' in col]
         daejeon_df.drop(columns=cols_to_drop, inplace=True)
 
         # 결과 저장
