e057fd517103794d4660d85377a5c83787ebdd91
seongho00 <tjdwns000927@gmail.com>
2025-07-25 12:39:41 +0900

    cluster 분석

--- TMRTeamProjectPython/commercial_cluster/commercial_cluster.py
+++ TMRTeamProjectPython/commercial_cluster/commercial_cluster.py
@@ -3,6 +3,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import matplotlib
+from sklearn.decomposition import PCA
+
+
 
 # 한글 폰트 설정 (Windows: 맑은 고딕)
 matplotlib.rcParams['font.family'] = 'Malgun Gothic'
@@ -12,6 +15,14 @@ matplotlib.rcParams['axes.unicode_minus'] = False
 
 # 분석할 업종 데이터 불러오기
 df = pd.read_csv('C:/Users/admin/Desktop/업종별_병합결과/CS100001_한식음식점.csv')
+
+# 클러스터 번호에 대한 설명
+cluster_labels = {
+    0: '거주지 중심 지역 (외곽형 상권)',
+    1: '복합생활 상권 (대학가·주거·소득 적정)',
+    2: '직장/주거 혼합 지역 (생활 밀착형)',
+    3: '중심 상업지구 (핵심 중심지)'
+}
 
 selected_features = [
     # 점포/경쟁 정보
@@ -58,9 +69,24 @@ plt.show()
 # 실제 군집 분석
 kmeans = KMeans(n_clusters=4, random_state=42)
 df['cluster'] = kmeans.fit_predict(X)
+df['cluster_label'] = df['cluster'].map(cluster_labels)
 
 # ✅ 결과 저장
-df.to_csv('C:/Users/admin/Desktop/업종별_병합결과_클로스터링/CS100001_한식음식점_클러스터링.csv', index=False, encoding='utf-8-sig')
+df.to_csv('C:/Users/admin/Desktop/업종별_병합결과_클로스터링/CS100001_한식음식점_클러스터링_라벨포함.csv', index=False, encoding='utf-8-sig')
 
 # ✅ 클러스터별 평균 확인
 print(df.groupby('cluster')[selected_features].mean())
+print(df[df['cluster'] == 3]['행정동_코드_명'].value_counts().head(10))
+
+df.groupby('cluster')[selected_features].mean().to_csv('C:/Users/admin/Desktop/클러스터_요약.csv', encoding='utf-8-sig')
+
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
+
+plt.figure(figsize=(8, 6))
+plt.scatter(X_pca[:, 0], X_pca[:, 1], c=df['cluster'], cmap='tab10', alpha=0.7)
+plt.title('PCA 시각화 (군집 분포)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.colorbar(label='Cluster')
+plt.show()
