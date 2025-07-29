ddc64472123465118f54e0f5f4db04e67ed5056f
hwshin1 <hwshin5044@gmail.com>
2025-07-29 12:50:44 +0900

    지도에 마우스 클릭시 정보 보임

--- src/main/resources/static/js/kakaomap.js
+++ src/main/resources/static/js/kakaomap.js
@@ -1,39 +1,36 @@
-var mapContainer = document.getElementById('map'),
-    mapOption = {
-        center: new kakao.maps.LatLng(37.566826, 126.9786567),
-        level: 7
-    };
-
+var mapContainer = document.getElementById('map');
+var mapOption = {
+    center: new kakao.maps.LatLng(37.566826, 126.9786567),
+    level: 7
+};
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
-// 등급별 색상
-const colorMap = {
-    1: '#ffffb2',
-    2: '#fecc5c',
-    3: '#fd8d3c',
-    4: '#f03b20',
-    5: '#bd0026'
-};
+const infowindow = new kakao.maps.InfoWindow();
 
+const path = "/js/seoul_area.geojson";
+
+// GeoJSON 파일 로드
 $.getJSON(path, function (geojson) {
-    geojson.features.forEach()
-    if (geojson.type === "GeometryCollection" && Array.isArray(geojson.geometries)) {
-        geojson.geometries.forEach((geometry) => {
-            if (geometry.type === "Polygon") {
-                drawPolygon(geometry.coordinates[0]);
-            } else if (geometry.type === "MultiPolygon") {
-                geometry.coordinates.forEach(polygon => {
-                    drawPolygon(polygon[0]);
-                });
-            }
-        });
-    } else {
-        console.error("GeometryCollection 형식이 아닙니다.");
-    }
+    const features = geojson.features;
+
+    features.forEach(feature => {
+        const coords = feature.geometry.coordinates;
+        const type = feature.geometry.type;
+        const fillColor = feature.properties.color;
+        const properties = feature.properties;
+
+        if (type === "Polygon") {
+            drawPolygon(coords[0], fillColor, properties);
+        } else if (type === "MultiPolygon") {
+            coords.forEach(polygon => {
+                drawPolygon(polygon[0], fillColor, properties);
+            });
+        }
+    });
 });
 
-function drawPolygon(coords, name) {
-    const path = coords.map(coord => new kakao.maps.LatLng(coord[1], coord[0]));
+function drawPolygon(coordArray, fillColor, properties) {
+    const path = coordArray.map(coord => new kakao.maps.LatLng(coord[1], coord[0]));
 
     const polygon = new kakao.maps.Polygon({
         map: map,
@@ -41,17 +38,24 @@ function drawPolygon(coords, name) {
         strokeWeight: 1,
         strokeColor: '#004c80',
         strokeOpacity: 0.8,
-        strokeStyle: 'solid',
-        fillColor: '#fff',
-        fillOpacity: 0
+        fillColor: fillColor,
+        fillOpacity: 0.6
     });
 
+    // 클릭 시 정보창
     kakao.maps.event.addListener(polygon, 'click', function () {
-        const content = `<div style="padding:8px;"><b>${name}</b></div>`;
-        const infowindow = new kakao.maps.InfoWindow({
-            content: content,
-            position: path[0]
-        });
+        const content = `
+            <div style="padding: 2px;">
+                <b>행정동:</b> ${properties.ADSTRD_NM}<br>
+                <b>매출 등급:</b> ${properties.매출_등급}<br>
+                <b>총 유동인구:</b> ${Number(properties.총_유동인구_수).toLocaleString()}명<br>
+                <b>당월 매출액:</b> ${Number(properties.당월_매출_금액).toLocaleString()}원
+            </div>
+        `;
+
+        // 새로 열기
+        infowindow.setContent(content);
+        infowindow.setPosition(path[0]);
         infowindow.open(map);
     });
 }
