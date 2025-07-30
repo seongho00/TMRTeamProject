4453d7b60d5a73c7902a4bab4613730c693ddf4e
hwshin1 <hwshin5044@gmail.com>
2025-07-30 16:45:05 +0900

    상관분석 데이터셋 자세히 병합 및 header 수정

--- src/main/java/com/koreait/exam/tmrteamproject/repository/UpjongCodeRepository.java
+++ src/main/java/com/koreait/exam/tmrteamproject/repository/UpjongCodeRepository.java
@@ -1,11 +1,8 @@
 package com.koreait.exam.tmrteamproject.repository;
 
-
-import com.koreait.exam.tmrteamproject.vo.AdminDong;
 import com.koreait.exam.tmrteamproject.vo.UpjongCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
--- src/main/java/com/koreait/exam/tmrteamproject/vo/AdminDong.java
+++ src/main/java/com/koreait/exam/tmrteamproject/vo/AdminDong.java
@@ -18,15 +18,18 @@ import javax.persistence.Id;
 @ToString(callSuper = true)
 public class AdminDong {
     @Id
-    @Column(name = "emd_cd")
-    private String emdCd;
-
+    @Column(name = "admi_nm")
+    private String admiNm;
+    @Column(name = "sido_cd")
+    private String sidoCd;
     @Column(name = "sido_nm")
     private String sidoNm;
     @Column(name = "sgg_cd")
     private String sggCd;
     @Column(name = "sgg_nm")
     private String sggNm;
+    @Column(name = "emd_cd")
+    private String emdCd;
     @Column(name = "emd_nm")
     private String emdNm;
 }
