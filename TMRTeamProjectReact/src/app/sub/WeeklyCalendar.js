a7863fd2c92892b3aabe0e346a1f6127ee4a8de3
seongho00 <tjdwns000927@gmail.com>
2025-08-01 16:42:54 +0900

    달력 npm 설치

---
+++ TMRTeamProjectReact/src/app/sub/WeeklyCalendar.js
@@ -0,0 +1,63 @@
+"use client";
+
+import { DateCalendar, PickersDay, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import koLocale from 'date-fns/locale/ko'; // 한국어 달력 원할 경우
+import { isWithinInterval } from 'date-fns';
+
+function getWeeksOfMonth(year, month) {
+    const weeks = [];
+    const date = new Date(year, month - 1, 1);
+    const lastDay = new Date(year, month, 0).getDate();
+
+    let weekStart = 1;
+
+    while (weekStart <= lastDay) {
+        const startDate = new Date(year, month - 1, weekStart);
+        const startDay = startDate.getDay();
+        const endDate = new Date(year, month - 1, Math.min(weekStart + (6 - startDay), lastDay));
+        weeks.push({ start: startDate, end: endDate });
+        weekStart = endDate.getDate() + 1;
+    }
+    return weeks;
+}
+
+const WeekHighlightDay = (props) => {
+    const { day, selectedWeekRange = {}, ...other } = props;
+    const isInSelectedWeek = isWithinInterval(day, {
+        start: selectedWeekRange.start,
+        end: selectedWeekRange.end,
+    });
+
+    return (
+        <PickersDay
+            {...other}
+            day={day}
+            sx={{
+                backgroundColor: isInSelectedWeek ? '#90caf9' : undefined,
+                borderRadius: '8px',
+            }}
+        />
+    );
+};
+
+const WeeklyCalendar = ({ year, month, weekInMonth }) => {
+    const selectedWeekRange = getWeeksOfMonth(year, month)[weekInMonth - 1];
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={koLocale}>
+            <DateCalendar
+                defaultValue={new Date(year, month - 1, 1)}
+                views={['day']}
+                disableHighlightToday
+                slots={{
+                    day: (dayProps) => (
+                        <WeekHighlightDay {...dayProps} selectedWeekRange={selectedWeekRange} />
+                    ),
+                }}
+            />
+        </LocalizationProvider>
+    );
+};
+
+export default WeeklyCalendar;
