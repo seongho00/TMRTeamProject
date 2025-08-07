2454f9f54977e4abc6c05445c725ac9696daf085
seongho00 <tjdwns000927@gmail.com>
2025-08-07 12:28:07 +0900

    보증금, 월세, 관리비 평균 계산

--- src/main/java/com/koreait/exam/tmrteamproject/repository/CommercialPropertyRepository.java
+++ src/main/java/com/koreait/exam/tmrteamproject/repository/CommercialPropertyRepository.java
@@ -4,6 +4,9 @@ import com.koreait.exam.tmrteamproject.vo.CommercialProperty;
 import com.koreait.exam.tmrteamproject.vo.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommercialPropertyRepository extends JpaRepository<CommercialProperty, Long> {
 
+    List<CommercialProperty> findByPriceType(String priceType);
 }
