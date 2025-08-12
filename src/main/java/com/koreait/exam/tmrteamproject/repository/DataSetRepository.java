0e2fb094dba4d68dd5eaae4ed3577d50dae1ca3a
hwshin1 <hwshin5044@gmail.com>
2025-08-12 13:16:49 +0900

    DB 저장 됨, 기준_년분기_코드 0 출력 오류

---
+++ src/main/java/com/koreait/exam/tmrteamproject/repository/DataSetRepository.java
@@ -0,0 +1,7 @@
+package com.koreait.exam.tmrteamproject.repository;
+
+import com.koreait.exam.tmrteamproject.vo.DataSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DataSetRepository extends JpaRepository<DataSet, Long> {
+}
