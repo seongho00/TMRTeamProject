a0bf5ec928c229db67468a32253ac8e4913fa5a8
seongho00 <tjdwns000927@gmail.com>
2025-08-13 11:04:53 +0900

    사용자 입력에 따른 코드 발급 구현

---
+++ src/main/java/com/koreait/exam/tmrteamproject/vo/AddressPickReq.java
@@ -0,0 +1,14 @@
+package com.koreait.exam.tmrteamproject.vo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+// DTO: 사용자가 고른 항목 + 동/호
+@Getter
+@Setter
+public class AddressPickReq {
+    private NormalizedAddress selected;
+    private String dong; // "101동" 가능
+    private String ho;   // "202호" 가능
+}
