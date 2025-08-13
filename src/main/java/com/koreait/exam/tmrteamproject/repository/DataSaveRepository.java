f1e77010f27e26f0ab9dca9de25cdd710c7daa55
hwshin1 <hwshin5044@gmail.com>
2025-08-13 11:48:42 +0900

    병합 csv DB 저장 완료, excel.html 수정

--- src/main/java/com/koreait/exam/tmrteamproject/repository/DataSaveRepository.java
+++ src/main/java/com/koreait/exam/tmrteamproject/repository/DataSaveRepository.java
@@ -1,7 +1,8 @@
 package com.koreait.exam.tmrteamproject.repository;
 
-import com.koreait.exam.tmrteamproject.vo.ClosedBiz;
+import com.koreait.exam.tmrteamproject.vo.DataSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DataSaveRepository extends JpaRepository<ClosedBiz, Long> {
+public interface DataSaveRepository extends JpaRepository<DataSet, Long> {
+    boolean existsByBaseYearQuarterCodeAndAdminDongCodeAndServiceIndustryCode(String baseYearQuarterCode, String adminDongCode, String serviceIndustryCode);
 }
--- src/main/java/com/koreait/exam/tmrteamproject/repository/DataSetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.koreait.exam.tmrteamproject.repository;
-
-import com.koreait.exam.tmrteamproject.vo.DataSet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataSetRepository extends JpaRepository<DataSet, Long> {
-}
