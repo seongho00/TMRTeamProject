328967a37d4ff48a49e69fac7b4600b65b7d2b82
hwshin1 <hwshin5044@gmail.com>
2025-08-21 12:08:12 +0900

    웹소켓 STOMP 등록 후 웹 알림창 띄우기만 메세지 x

--- src/main/java/com/koreait/exam/tmrteamproject/scheduler/SmsScheduler.java
+++ src/main/java/com/koreait/exam/tmrteamproject/scheduler/SmsScheduler.java
@@ -3,12 +3,15 @@ package com.koreait.exam.tmrteamproject.scheduler;
 import com.koreait.exam.tmrteamproject.repository.LhSupplyScheduleRepository;
 import com.koreait.exam.tmrteamproject.repository.MemberRepository;
 import com.koreait.exam.tmrteamproject.repository.ScheduleInterestRepository;
+import com.koreait.exam.tmrteamproject.service.NotificationService;
 import com.koreait.exam.tmrteamproject.service.SolapiSmsService;
 import com.koreait.exam.tmrteamproject.util.PhoneNumber;
+import com.koreait.exam.tmrteamproject.vo.DueAlert;
 import com.koreait.exam.tmrteamproject.vo.LhSupplySchedule;
 import com.koreait.exam.tmrteamproject.vo.Member;
 import com.koreait.exam.tmrteamproject.vo.ScheduleInterest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -18,13 +21,16 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SmsScheduler {
+
     private final LhSupplyScheduleRepository lhSupplyScheduleRepository;
     private final SolapiSmsService smsService;
     private final ScheduleInterestRepository scheduleInterestRepository;
     private final MemberRepository memberRepository;
+    private final NotificationService notificationService;
 
-    @Scheduled(cron = "0 0 10 * * *") // 시간설정
+    @Scheduled(fixedDelay = 10000) // 시간설정
     public void checkAndSendSms() {
         LocalDate today = LocalDate.now();
 
@@ -66,8 +72,23 @@ public class SmsScheduler {
             }
 
             if (msg != null && si.getIsActive() == 1) {
+
+                /* SNS 보내기 (전송 보유 토큰 무료 분 다 씀)
                 smsService.sendSms(phoneNum, msg);
-                System.out.println("문자 전송 완료 ->" + phoneNum + " / " + msg);
+                log.info("문자 전송 완료 -> {} / {}", phoneNum, msg);
+                 */
+
+                // 웹 팝업 알림창
+                DueAlert dueAlert = DueAlert.builder()
+                        .scheduleId(lhSupplySchedule.getId())
+                        .name(lhSupplySchedule.getName())
+                        .applyStart(lhSupplySchedule.getApplyStart())
+                        .daysUtil((int) daysUntil)
+                        .msg(msg)
+                        .build();
+
+                notificationService.notifyMember(si.getMemberId(), dueAlert);
+                log.info("웹 알림 전송 완료 -> memberId={}, msg={}", si.getMemberId(), msg);
             }
         }
 
