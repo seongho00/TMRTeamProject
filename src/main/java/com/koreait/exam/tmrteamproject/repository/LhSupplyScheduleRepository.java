3f29e1d3e9366a1dbe00173932b8590faab0e9e7
hwshin1 <hwshin5044@gmail.com>
2025-09-10 11:40:31 +0900

    로그인 세션 관리 및 로그인시 / 로그아웃시 보여주는거 다르게

--- src/main/java/com/koreait/exam/tmrteamproject/repository/LhSupplyScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.koreait.exam.tmrteamproject.repository;
-
-import com.koreait.exam.tmrteamproject.vo.AdminDong;
-import com.koreait.exam.tmrteamproject.vo.LhSupplySchedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LhSupplyScheduleRepository extends JpaRepository<LhSupplySchedule, Long> {
-
-}
