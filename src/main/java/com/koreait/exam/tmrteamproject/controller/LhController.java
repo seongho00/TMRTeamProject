45f1c606e337527d9352d163fa4c6d36d6a76eab
seongho00 <tjdwns000927@gmail.com>
2025-09-11 15:52:30 +0900

    알림 설정 버튼 DB 연결

--- src/main/java/com/koreait/exam/tmrteamproject/controller/LhController.java
+++ src/main/java/com/koreait/exam/tmrteamproject/controller/LhController.java
@@ -1,14 +1,16 @@
 package com.koreait.exam.tmrteamproject.controller;
 
+import com.koreait.exam.tmrteamproject.repository.LhApplyInfoRepository;
+import com.koreait.exam.tmrteamproject.security.MemberContext;
+import com.koreait.exam.tmrteamproject.service.ScheduleInterestService;
 import com.koreait.exam.tmrteamproject.vo.LhApplyInfo;
 import com.koreait.exam.tmrteamproject.service.LhApplyInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,18 +21,40 @@ import java.util.List;
 public class LhController {
 
     private final LhApplyInfoService lhApplyInfoService;
+    private final ScheduleInterestService scheduleInterestService;
 
     @GetMapping
     public String notice(
-            @RequestParam(required = false, defaultValue = "")String type,
-            @RequestParam(required = false, defaultValue = "")String region,
-            @RequestParam(required = false, defaultValue = "")String status,
-            @RequestParam(required = false, defaultValue = "")String q,
-            Model model) {
+            @RequestParam(required = false, defaultValue = "") String type,
+            @RequestParam(required = false, defaultValue = "") String region,
+            @RequestParam(required = false, defaultValue = "") String status,
+            @RequestParam(required = false, defaultValue = "") String q,
+            Model model, @AuthenticationPrincipal MemberContext memberContext) {
+
+        Long memberId = null;
+        if (memberContext != null) {
+            memberId = memberContext.getMember().getId();
+        }
 
         List<LhApplyInfo> lhApplyInfoList = lhApplyInfoService.searchNotices(type, region, status, q);
 
         model.addAttribute("lhApplyInfoList", lhApplyInfoList);
+        model.addAttribute("memberId", memberId); // 로그인 X → null 들어감
         return "subscription/noticeList";
+    }
+
+    // 알림설정하기
+    @PostMapping
+    @ResponseBody
+    public String saveSchedule(long scheduleId, long memberId) {
+
+
+        System.out.println("실행됨");
+        System.out.println("scheduleId:" + scheduleId);
+        System.out.println("memberId:" + memberId);
+
+//        scheduleInterestService.saveSchedule(null, 1);
+
+        return "subscription/noticeAdd";
     }
 }
